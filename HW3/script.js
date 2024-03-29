0278b8a8f72b5dc75f14c72229c1e4c13df9bd56
chihyii <160991192+chihyii@users.noreply.github.com>
2024-03-29 11:21:38 +0800

    Update script.js

--- HW3/script.js
+++ HW3/script.js
@@ -1,8 +1,8 @@
 // 定義變數
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-let x1 = 0, y1 = 0, dx1 = 5, dy1 = 5, r1 = 70, color1 = "#81C0C0";
-let x2 = canvas.width, y2 = 0, dx2 = 10, dy2 = 5, r2 = 80, color2 = "#AAAAFF"
+let x1 = 80, y1 = 80, dx1 = 5, dy1 = 5, r1 = 70, color1 = "#81C0C0";
+let x2 = 200, y2 = 200, dx2 = -5, dy2 = 5, r2 = 80, color2 = "#AAAAFF"
 let ndx1 = 0, ndy1 = 0, ndx2 = 0, ndy2 = 0
 // 畫圓形
 function drawBall(x, y, r, color) {
@@ -24,24 +24,25 @@ function draw() {
   y2 = y2 + dy2;
 
   // TODO: 如果發生碰撞(畫布寬canvas.width, 畫布高canvas.height)，則改變速度(dx, dy)和顏色(color)
-  if (x1 < 0 || x1 > canvas.width) {
+  if (x1-r1 < 0 || x1+r1 > canvas.width) {
     dx1 = -dx1;
     color1 = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
-  if (y1 < 0 || y1 > canvas.height) {
+  if (y1-r1 < 0 || y1+r1 > canvas.height) {
     dy1 = -dy1;
     color1 = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
-  if (x2 < 0 || x2 > canvas.width) {
+  if (x2-r2 < 0 || x2+r2 > canvas.width) {
     dx2 = -dx2;
     color2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
-  if (y2 < 0 || y2 > canvas.height) {
+  if (y2-r2 < 0 || y2+r2 > canvas.height) {
     dy2 = -dy2;
     color2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
 
-  if ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) < (r1 + r2) * (r1 + r2)) {
+  if ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) < (r1 + r2) * (r1 + r2)) 
+  {
     ndx1 = ((r1 - r2) * dx1 + 2 * r2 * dx2) / (r1 + r2);
     ndx2 = ((r2 - r1) * dx2 + 2 * r1 * dx1) / (r1 + r2);
     ndy1 = ((r1 - r2) * dy1 + 2 * r2 * dy2) / (r1 + r2);
